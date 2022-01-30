e520b34a262a7b14f8b870b74f5cb92ae1eeac3d
Yurii Mochalov <ym@anadeainc.com>
2022-01-30 19:07:51 +0200

    Add model dependencies

--- server/models/models.js
+++ server/models/models.js
@@ -18,9 +18,9 @@ const BasketDevice = sequelize.define('basket_device', {
 
 const Device = sequelize.define('device', {
   id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-  name: {type: DataTypes.STRING, unique: true, allowNull: false}
-  price: {type: DataTypes.INTEGER, allowNull: false}
-  rating: {type: DataTypes.INTEGER, defaultValue: 0}
+  name: {type: DataTypes.STRING, unique: true, allowNull: false},
+  price: {type: DataTypes.INTEGER, allowNull: false},
+  rating: {type: DataTypes.INTEGER, defaultValue: 0},
   img: {type: DataTypes.STRING, allowNull: false}
 })
 
@@ -41,6 +41,49 @@ const Rating = sequelize.define('rating', {
 
 const DeviceInfo = sequelize.define('device_info', {
   id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-  title: {type: DataTypes.STRING, allowNull: false}
+  title: {type: DataTypes.STRING, allowNull: false},
   description: {type: DataTypes.STRING, allowNull: false}
 })
+
+const TypeBrand = sequelize.define('type_brand', {
+  id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+})
+
+User.hasOne(Basket)
+Basket.belongsTo(User)
+
+User.hasMany(Rating)
+Rating.belongsTo(User)
+
+Basket.hasMany(BasketDevice)
+BasketDevice.belongsTo(Basket)
+
+Type.hasMany(Device)
+Device.belongsTo(Type)
+
+Brand.hasMany(Device)
+Device.belongsTo(Brand)
+
+Device.hasMany(Rating)
+Rating.belongsTo(Device)
+
+Device.hasMany(BasketDevice)
+BasketDevice.belongsTo(Device)
+
+Device.hasMany(DeviceInfo)
+DeviceInfo.belongsTo(Device)
+
+Type.belongsToMany(Brand, {through: TypeBrand})
+Brand.belongsToMany(Type, {through: TypeBrand})
+
+module.exports = {
+  User,
+  Basket,
+  BasketDevice,
+  Device,
+  Type,
+  Brand,
+  Rating,
+  TypeBrand,
+  DeviceInfo
+}
