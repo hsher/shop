f3149c19c3584fff8a5c52da803ab3ec203c0e3e
Yurii Mochalov <ym@anadeainc.com>
2022-06-08 19:54:02 +0300

    Add Post method for devices

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,6 +1,21 @@
+const uuid = require('uuid')
+const path = require('path')
+const {Device} = require('../models/models')
+const ApiError = require('../error/ApiError')
 class DeviceController {
-  async create(req, res) {
+  async create(req, res, next) {
+    try {
+      const {name, price, brandId, typeId, indo} = req.body
+      const {img} = req.files
+      let fileName = uuid.v4() + ".jpg"
+      img.mv(path.resolve(__dirname, '..', 'static', fileName))
 
+      const devices = await Device.create({name, price, brandId, typeId, img: fileName})
+
+      return res.json(devices)
+    } catch {
+      next(ApiError.badRequest(e.message))
+    }
   }
 
   async getAll(req, res) {
