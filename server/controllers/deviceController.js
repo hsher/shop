63b68020a65130b0e70252535a8bc53024cdac55
Yurii Mochalov <ym@anadeainc.com>
2022-06-10 11:44:03 +0300

    Add GET one device with info field

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -51,7 +51,15 @@ class DeviceController {
   }
 
   async getOne(req, res) {
+    const {id} = req.params
+    const device = await Device.findOne(
+      {
+        where: {id},
+        include: [{model: DeviceInfo, as: 'info'}]
+      },
+    )
 
+    return res.json(device)
   }
 }
 
