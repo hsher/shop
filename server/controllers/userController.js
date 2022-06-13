803f7c0fb5839ce5fd2ae306fa3a7f7adb68e6e3
Yurii Mochalov <ym@anadeainc.com>
2022-06-13 12:55:34 +0300

    Add login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -33,8 +33,23 @@ class UserController {
     return res.json({token})
   }
 
-  async login(req, res) {
+  async login(req, res, next) {
+    const {email, password} = req.body
+    const user = await User.findOne({where: {email}})
 
+    if (!user) {
+      return next(ApiError.internal('Пользователь с таким именем не найден'))
+    }
+
+    let comparePassword = bcrypt.compareSync(password, user.password)
+
+    if (!comparePassword) {
+      return next(ApiError.internal('Указан неверный пароль'))
+    }
+
+    const token = generateJwt(user.id, user.email, user.role)
+
+    return res.json({token})
   }
 
   async check(req, res, next) {
