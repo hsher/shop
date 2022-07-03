b0539e1668a3d8926a0a6154feb3570c93bb181c
Yurii Mochalov <ym@anadeainc.com>
2022-07-03 18:45:27 +0300

    Add device fields

--- client/src/components/modals/CreateDevice.js
+++ client/src/components/modals/CreateDevice.js
@@ -1,7 +1,10 @@
-import React from 'react';
-import {Button, Form, Modal} from 'react-bootstrap';
+import React, {useContext, useState} from 'react';
+import {Button, Col, Dropdown, Form, Modal, Row} from 'react-bootstrap';
+import {Context} from '../../index';
 
 const CreateDevice = ({show, onHide}) => {
+  const {device} = useContext(Context)
+
   return (
     <Modal
       show={show}
@@ -16,9 +19,49 @@ const CreateDevice = ({show, onHide}) => {
       </Modal.Header>
       <Modal.Body>
         <Form>
+          <Dropdown className="mt-2 mb-2">
+            <Dropdown.Toggle>
+              Выберите тип
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              {device.types.map(type =>
+                <Dropdown.Item key={type.id}>
+                  {type.name}
+                </Dropdown.Item>
+              )}
+            </Dropdown.Menu>
+          </Dropdown>
+
+          <Dropdown className="mt-2 mb-2">
+            <Dropdown.Toggle>
+              Выберите бренд
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              {device.brands.map(brand =>
+                <Dropdown.Item key={brand.id}>
+                  {brand.name}
+                </Dropdown.Item>
+              )}
+            </Dropdown.Menu>
+          </Dropdown>
+
           <Form.Control
-            placeholder={"Введите название типа"}
+            className="mt-3"
+            placeholder="Введите название устройства"
           />
+
+          <Form.Control
+            className="mt-3"
+            placeholder="Введите стоимость устройства"
+            type="number"
+          />
+
+          <Form.Control
+            className="mt-3"
+            type="file"
+          />
+
+          <hr />
         </Form>
       </Modal.Body>
       <Modal.Footer>
