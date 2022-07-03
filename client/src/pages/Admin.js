bdf5ea30f4a0a6ed6821d7a16731a9c9198ce0bc
Yurii Mochalov <ym@anadeainc.com>
2022-07-03 18:02:47 +0300

    Add Admin page base structure

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import {Button, Container} from 'react-bootstrap';
 
 const Admin = () => {
   return (
-    <div>
-      Admin
-    </div>
+    <Container>
+      <div className="d-flex flex-column">
+        <Button variant={"outline-dark"} className="mt-4 p-2">
+          Добавить тип
+        </Button>
+        <Button variant={"outline-dark"} className="mt-4 p-2">
+          Добавить бренд
+        </Button>
+        <Button variant={"outline-dark"} className="mt-4 p-2">
+          Добавить устрйство
+        </Button>
+      </div>
+    </Container>
   )
 }
 
