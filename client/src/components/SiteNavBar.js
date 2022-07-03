6a3b18af5f6a1f40e5d998c110bb3c22f3379c61
Yurii Mochalov <ym@anadeainc.com>
2022-07-03 16:27:45 +0300

    Update navigation links

--- client/src/components/SiteNavBar.js
+++ client/src/components/SiteNavBar.js
@@ -3,13 +3,15 @@ import {Context} from '../index';
 import NavBar from 'react-bootstrap/NavBar';
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
-import {NavLink} from 'react-router-dom';
+import {NavLink, useNavigate} from 'react-router-dom';
 import {SHOP_ROUTE} from '../utils/consts';
 import {observer} from 'mobx-react-lite';
 import Container from 'react-bootstrap/Container'
+import {ADMIN_ROUTE, LOGIN_ROUTE} from '../utils/consts';
 
 const SiteNavBar = observer(() => {
   const {user} = useContext(Context)
+  const navigate = useNavigate()
 
   return (
     <NavBar bg="dark" variant="dark" className="justify-content-between">
@@ -17,8 +19,19 @@ const SiteNavBar = observer(() => {
         <NavLink style={{color: 'white'}} to={SHOP_ROUTE}>КупиДевайс</NavLink>
         {user.isAuth ?
           <Nav style={{color: 'white'}}>
-            <Button variant={"outline-light"}>Админ панель</Button>
-            <Button variant={"outline-light"} style={{"margin-left": "0.5rem"}} onClick={() => user.setIsAuth(false)}>Выйти</Button>
+            <Button
+              variant={"outline-light"}
+              onClick={() => navigate(ADMIN_ROUTE)}
+            >
+              Админ панель
+            </Button>
+            <Button
+              variant={"outline-light"}
+              style={{"margin-left": "0.5rem"}}
+              onClick={() => {user.setIsAuth(false); navigate(LOGIN_ROUTE)}}
+            >
+              Выйти
+            </Button>
           </Nav>
           :
           <Nav style={{color: 'white'}}>
