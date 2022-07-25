d108b9869cc022d3876a711d35944f6548995807
Yurii Mochalov <ym@anadeainc.com>
2022-07-25 12:00:24 +0300

    Use correct path for user.isAuth checking

--- client/src/http/userAPI.js
+++ client/src/http/userAPI.js
@@ -16,7 +16,7 @@ export const login = async (email, password) => {
 }
 
 export const check = async () => {
-  const {data} = await $authHost.get('http://localhost:5000/api/user')
+  const {data} = await $authHost.get('http://localhost:5000/api/user/auth')
   localStorage.setItem('token', data.token)
 
   return jwt.decode(data.token)
